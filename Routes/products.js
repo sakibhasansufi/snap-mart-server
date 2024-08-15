2aa8eb4d3da2baf8c9b22c491c34625468898b6f
sakibhasansufi <sakibhasansufi@gmail.com>
2024-08-15 16:49:21 +0600

    sort method applied in the backend

---
+++ Routes/products.js
@@ -0,0 +1,83 @@
+const router = require("express").Router();
+const Product = require("../Models/Product.js")
+
+
+router.get("/movie", async (req, res) => {
+    try {
+        const page = parseInt(req.query.page) - 1 || 0;
+        const limit = parseInt(req.query.limit) || 5;
+        const search = req.query.search || "";
+        let sort = req.query.sort || "date";
+        let category = req.query.category || "All";
+
+        const categoryOptions = [
+            "Processor",
+            "GPU",
+            "Monitor",
+            "Motherboard",
+            "RAM",
+            "SSD",
+            "HDD",
+            "Cooler",
+            "Power Supply",
+            "Case",
+            "Keyboard",
+            "Mouse",
+            "Headphone",
+            "Mouse Pad",
+            "Speaker",
+            "Headphone",
+            "Microphone",
+            "UPS",
+            "Others"
+        ];
+
+
+        category === "All"
+            ? (category = [...categoryOptions])
+            : (category = req.query.category.split(","));
+
+        req.query.sort ? (sort = req.query.sort.split(",")) : (sort = [sort]);
+
+        let sortBy = {};
+        if (sort[1]) {
+            sortBy[sort[0]] = sort[1];
+        } else {
+            sortBy[sort[0]] = 'asc'
+        }
+
+        const products = await Product.find({ name: { $regex: search, options: "i" } })
+            .where("category")
+            .in([...category])
+            .sort(sortBy)
+            .skip(page * limit)
+            .limit(limit)
+
+
+        const total = await Product.countDocuments({
+            category: { $in: [...category] },
+            name: { $regex: search, options: "i" }
+        })
+
+        const response = {
+			error: false,
+			total,
+			page: page + 1,
+			limit,
+			category: categoryOptions,
+			products,
+		};
+
+        res.status(200).json(response);
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            error: true,
+            message: "Internal server error"
+        })
+    }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -3,18 +3,23 @@ const express = require('express');
 const dbConnect= require("./dbConnect");
 const cors = require('cors');
 const app = express();
+const productRoutes = require("./Routes/products.js")
 
 dbConnect();
 
 app.use(express.json());
 app.use(cors());
 
+app.use("/api",productRoutes);
+
 
 const port = process.env.PORT || 6000;
 
 app.get('/', (req, res) => {
     res.send("Server is running")
-})
+});
+
+
 
 
 app.listen(port, () => {
